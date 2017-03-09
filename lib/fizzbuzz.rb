d04da135ac766ce53728807aca357e0217693b3a
Emily Chai <echai93@gmail.com>
2017-03-09 17:51:57 +0000

    Added tests and modified production code to meet the specs

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,12 @@
-def fizzbuzz
-end 
+def fizzbuzz(number)
+  if number == 0
+    return number
+  elsif number % 3 == 0 && number % 5 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  elsif number % 5 == 0
+    return 'buzz'
+  else number
+  end
+end
